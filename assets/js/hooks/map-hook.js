cdd3bac53c5f70edb328119ae43292f777fa6615
mbnissen <morten@endavu.com>
2025-02-09 09:26:14 +0100

    Some css

--- assets/js/hooks/map-hook.js
+++ assets/js/hooks/map-hook.js
@@ -9,18 +9,44 @@ const MapHook = {
     const map = new mapboxgl.Map({
       container: this.el.id,
       zoom: 13,
-      center: [12.547187368954477, 55.66598654676102] // [lng, lat]
+      center: [
+        12.569157292471003,
+        55.67622541130143
+      ]
+    });
+
+    // Add geolocate control to the map.
+    const geolocate = map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true
+      })
+    );
+
+    // Add the control to the map.
+    map.addControl(geolocate);
+    map.on('load', () => {
+        geolocate.trigger();
     });
 
     for (const location of locations) {
+      // create a HTML element for each feature
+      const el = document.createElement('div');
+      el.className = 'marker';
+
       const html = `<div>
         <h1 class="text-lg font-semibold">${location.name}</h1>
       </div>`;
 
       // Create a new marker.
-      const marker = new mapboxgl.Marker()
+      const marker = new mapboxgl.Marker(el)
         .setLngLat([location.lng, location.lat])
-        .setPopup(new mapboxgl.Popup().setHTML(html))
+        .setPopup(new mapboxgl.Popup({offset: 25}).setHTML(html))
         .addTo(map);
 
       marker.togglePopup(); // toggle popup open or closed
